d4c4676d20474f85b1694c987847d7a5e26dc2af
zheephy <Lirilara123>
2022-04-02 22:03:08 -0500

    Leap Year Class

---
+++ SalesSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SalesSystem;
+
+Console.WriteLine("=============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 31));
+    Console.WriteLine(new Date(2024, 02, 29));
+    Console.WriteLine(new Date(2022, 01, 5));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
